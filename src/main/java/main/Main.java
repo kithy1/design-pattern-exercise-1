3656622ec67e865c49429ca6051d37dbd8856b9d
Katarzyna <kithy@interia.eu>
2019-02-17 12:08:29 +0100

    implements fluentbuilder

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -7,5 +7,6 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         BarChart barChart = new BarChart("Task types report",new ArrayList());
+        BarChart barChart1 = new BarChart.BarChartBuilder().withTitle("tytuł").build();
     }
 }
